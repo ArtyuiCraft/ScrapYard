56c7753493f46a62e092c3a4a5d5a01c298a008f
ArtyuiCraft <ArtyuiCraft@gmail.com>
2024-12-22 12:37:25 +0100

    rockkkk

--- Objects/scrap_node.gd
+++ Objects/scrap_node.gd
@@ -1,11 +1,11 @@
 @tool
 extends Node2D
 
-@export var images: Array[Texture2D]
+var rotations = ["up",'down','left','right']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$BlackBox.texture = images.pick_random()
+	$BlackBox.rotation_degrees = Utils.string_to_rotation(rotations.pick_random())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
