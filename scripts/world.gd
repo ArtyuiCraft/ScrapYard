01f00a55ba1d80dc98edce5c9b0648ba74020907
ArtyuiCraft <ArtyuiCraft@gmail.com>
2024-12-22 13:23:33 +0100

    breaking animation

--- scripts/world.gd
+++ scripts/world.gd
@@ -6,6 +6,8 @@ extends Node2D
 @export var ghost: Color
 var mouserotation = 0
 @export var scene: PackedScene
+var break_mouse_pos
+var inbreak = true
 
 func _process(delta):
 	if Input.is_action_just_pressed("rotate+"):
@@ -36,8 +38,25 @@ func _process(delta):
 		else:
 			if $World.get_cell_atlas_coords(mouse_pos) == Vector2i(-1,-1) and !($World.local_to_map(player.position) == mouse_pos):
 				$Objects.set_cell(mouse_pos, 0,Vector2i(0,0),player.hotbar[player.selected_item % len(player.hotbar)])
-	if Input.is_action_pressed("break"):
+			else:
+				print("as")
+	if !$BreakTimer.is_stopped():
+		$GuiLayer/gui/ProgressBar.value = ($BreakTimer.wait_time - $BreakTimer.time_left) * 1000
+	else:
+		$GuiLayer/gui/ProgressBar.value = 0
+	if Input.is_action_just_pressed("break"):
+		$BreakTimer.start()
+		break_mouse_pos = mouse_pos
+		inbreak = true
+	if Input.is_action_just_released("break"):
+		$BreakTimer.stop()
+		inbreak = false
+	if mouse_pos != break_mouse_pos and !$BreakTimer.is_stopped():
+		$BreakTimer.stop()
+		inbreak = false
+	if $BreakTimer.is_stopped() and inbreak:
 		$Objects.erase_cell(mouse_pos)
+		inbreak = false
 	#if Input.is_action_just_pressed("spawn"):
 	#	var new_scene = scene.instantiate()
 	#	add_child(new_scene)
